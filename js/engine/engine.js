11c995f64b1de152aa93a2843c33b3dcc3869fef
glumpo <>
2016-12-02 19:10:33 +0300

    input Для загрузки файлов и агрессивный title для поднятия духа.

--- js/engine/engine.js
+++ js/engine/engine.js
@@ -1,29 +1,3 @@
 define([/* no dependencies yet */], function() {
-    
-    
-    if ( !(window.File && window.FileReader && window.FileList && window.Blob) ){
-        //TODO: error handler and don`t humiliate user anymore...
-        alert("What the fuck with ur browser? UPDATE IT SONOFABITH!!!!");
-    }
-    
-    var control = document.getElementById("userInputFile");
-    var userObj;
-    
-    control.addEventListener("change", function (event) {
-        var userFile = control.files[0];
-
-        reader = new FileReader();
-        reader.readAsText(userFile);
-        
-        reader.onload = function(evt) {
-            window.userObj = JSON.parse(evt.target.result);
-            
-        };
-        
-        reader.onerror = function(evt) {
-            //TODO: error handler
-            alert("SOME ERROR WHILE READING JSON!!!");
-        };
-    });
 
 });
--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 requirejs.config({
-    baseUrl: 'js',
+    baseUrl: 'js'
 });
 
-requirejs(['engine/engine'], function(engine) {
-    alert("It's working!");
+requirejs(['engine/engine'], function (engine) {
+    if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
+        //TODO: error handler and don`t humiliate user anymore...
+        alert("What the fuck with ur browser? UPDATE IT SONOFABITH!!!!");
+    }
+
+    var control = document.getElementById("userInputFile");
+    var userObj;
+
+    control.addEventListener("change", function (event) {
+        var userFile = control.files[0];
+
+        reader = new FileReader();
+        reader.readAsText(userFile);
+
+        reader.onload = function (evt) {
+            window.userObj = JSON.parse(evt.target.result);
+
+        };
+
+        reader.onerror = function (evt) {
+            //TODO: error handler
+            alert("SOME ERROR WHILE READING JSON!!!");
+        };
+    });
 });
