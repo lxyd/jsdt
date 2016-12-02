ce2d3b31c6b05d4d1eea6e169f2212446c4722bc
glumpo <>
2016-12-02 18:47:56 +0300

    Наброски Зарузки файла и парсинга JSON в объект.

--- js/engine/engine.js
+++ js/engine/engine.js
@@ -1,3 +1,27 @@
 define([/* no dependencies yet */], function() {
+    
+    
+    if ( !(window.File && window.FileReader && window.FileList && window.Blob) ){
+        alert("What the fuck with ur browser? UPDATE IT SONOFABITH!!!!");  //TODO: error handler and don`t humiliate user anymore...
+    }
+    
+    var control = document.getElementById("userInputFile");
+    var userObj;
+    
+    control.addEventListener("change", function (event) {
+        var userFile = control.files[0];
+
+        reader = new FileReader();
+        reader.readAsText(userFile);
+        
+        reader.onload = function(evt) {
+            window.userObj = JSON.parse(evt.target.result);
+            
+        };
+        
+        reader.onerror = function(evt) {
+            alert("SOME ERROR WHILE READING JSON!!!");  //TODO: error handler
+        };
+    });
 
 });
